672fddefccdf331200917a1173928fff2f748a63
carsonuderitz@gmail.com <carsonuderitz@gmail.com>
2025-09-15 13:07:48 -0400

    density and volume

--- scripts/sph_sim.gd
+++ scripts/sph_sim.gd
@@ -4,6 +4,8 @@ extends Node3D
 
 @export var particle_count: int = 500
 @export var bounding_box_size: int = 20
+@export var smoothing_radius: float = 0.1
+@export var mass: float = 1.0
 
 
 @onready var _visualizer: MultiMeshSphVisualizer = $MultiMeshSphVisualizer
@@ -12,13 +14,15 @@ extends Node3D
 	-bounding_box_size, -bounding_box_size, -bounding_box_size
 )
 @onready var _c2 := _c1 * -1
+@onready var target_volume: float = smoothing_radius * particle_count
+@onready var target_density = (mass * particle_count) / target_volume
 
 
 var _particles: Array[Particle] = []
 
 
 func _ready() -> void:
-
+	print_debug(target_density)
 	randomize()
 	for i in range(particle_count):
 		@warning_ignore("NARROWING_CONVERSION")
