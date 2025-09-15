ab493f2684b5e3c808e668fa2bdae27c52531335
carsonuderitz@gmail.com <carsonuderitz@gmail.com>
2025-09-15 13:27:49 -0400

    kernels and calculating density

--- scripts/particle.gd
+++ scripts/particle.gd
@@ -3,9 +3,9 @@ extends RefCounted
 
 
 var position: Vector3
+var density: float = 0.0
+var pressure: float = 0.0
 
 
 func _init(_position: Vector3) -> void:
 	position = _position
-
-
--- scripts/sph_sim.gd
+++ scripts/sph_sim.gd
@@ -36,6 +36,48 @@ func _ready() -> void:
 		_particles.append(particle)
 
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
+	_update_simulation(delta)
 	if _has_visualizer:
 		_visualizer.render(_particles)
+
+
+func _update_simulation(_delta: float) -> void:
+	_calculate_densities()
+
+
+func _calculate_densities():
+	for i: int in range(particle_count):
+		_particles[i].density = _calculate_density(i)
+		print_debug(_particles[i].density)
+
+
+func _calculate_density(index: int) -> float:
+	var density := 0.0
+
+	var pos := _particles[index].position
+	for i: int in range(particle_count):
+		if i == index:
+			continue
+
+		var particle: Particle = _particles[i]
+		density += mass * poly6_kernel(pos.distance_squared_to(particle.position))
+
+	return density
+
+
+func poly6_kernel(d2: float) -> float:
+	var h2 := smoothing_radius**2
+	if d2 > h2:
+		return 0.0
+
+	var factor := (h2 - d2)**3
+	return (315/(64*PI*(smoothing_radius**9)))*factor
+
+
+func spiky_kernel(d: float) -> float:
+	if d > smoothing_radius:
+		return 0.0
+
+	var factor := (smoothing_radius - d)**3
+	return (15/(PI*(smoothing_radius**6)))*factor
